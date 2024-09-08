e7fa4f6c342dd423e75c57656efb1e5bfa3b4764
Martin Meny <martin.meny@alumnos.frgp.utn.edu.ar>
2024-09-08 20:53:07 -0300

    se crea conexion con base de datos mediante clase ArticuloNegocio

--- TP2WinFormsG21A/Articulo.cs
+++ TP2WinFormsG21A/Articulo.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace TP2WinFormsG21A
 {
     internal class Articulo
     {
+        public int codigoArticulo { get; set; }
+        public string nombreArticulo { get; set; }
+        public string descripcionArticulo { get; set; }
+        public int marcaArticulo { get; set; }
+        public int categoriaArticulo { get; set; }
+        public string imagenArticulo { get; set; }
+        public float precioArticulo { get; set; }
     }
 }
---
+++ TP2WinFormsG21A/ArticuloNegocio.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace TP2WinFormsG21A
+{
+    internal class ArticuloNegocio
+    {
+        public List<Articulo> listar() {
+
+            List<Articulo> lista = new List<Articulo>();
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand comando = new SqlCommand();
+            SqlDataReader lector;
+
+            try
+            {
+                conexion.ConnectionString = "server=.\\SQLEXPRESS; database=CATALOGO_P3_DB; integrated security=true;";
+                comando.CommandType = System.Data.CommandType.Text;
+                comando.CommandText = "select Id, Codigo, Nombre, Descripcion, IdMarca, IdCategoria, Precio From ARTICULOS";
+                comando.Connection = conexion;
+
+                conexion.Open();
+                lector = comando.ExecuteReader();
+
+                while (lector.Read())
+                {
+                    Articulo aux = new Articulo();
+                    aux.codigoArticulo = (int)lector["Id"];
+                    aux.nombreArticulo = (string)lector["Nombre"];
+                    aux.descripcionArticulo = (string)lector["Descripcion"];
+                    aux.marcaArticulo = (int)lector["Marca"];
+                    aux.categoriaArticulo = (int)lector["Categoria"];
+                    aux.imagenArticulo = (string)lector["Imagen"];
+                    aux.precioArticulo = (float)lector["Precio"];
+
+                    lista.Add(aux);
+                }
+
+
+                return lista;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            finally
+            {
+                conexion.Close();
+            }
+        
+        }
+        
+    }
+}
